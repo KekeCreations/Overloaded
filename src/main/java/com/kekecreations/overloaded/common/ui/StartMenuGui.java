fb2c123efe4813ec55e89390e5ba55c2a0048ae9
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2026-03-19 22:43:58 +0000

    UI Updates

--- src/main/java/com/kekecreations/overloaded/common/ui/StartMenuGui.java
+++ src/main/java/com/kekecreations/overloaded/common/ui/StartMenuGui.java
@@ -7,6 +7,7 @@ import com.hypixel.hytale.protocol.packets.camera.SetServerCamera;
 import com.hypixel.hytale.protocol.packets.interface_.CustomPageLifetime;
 import com.hypixel.hytale.protocol.packets.interface_.CustomUIEventBindingType;
 import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.entity.entities.player.pages.InteractiveCustomUIPage;
 import com.hypixel.hytale.server.core.ui.builder.EventData;
 import com.hypixel.hytale.server.core.ui.builder.UICommandBuilder;
@@ -15,10 +16,13 @@ import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 
 import javax.annotation.Nonnull;
+import java.util.Objects;
 
 public class StartMenuGui extends InteractiveCustomUIPage<StartMenuGuiData> {
 
-    private static final String PLAY_NORMAL_BUTTON_ID = "PLAYNORMAL";
+    private static final String PLAY_NORMAL_BUTTON_ID = "PLAYCLASSIC";
+    private static final String PLAY_QUICK_BUTTON_ID = "PLAYQUICK";
+    private static final String QUIT_BUTTON_ID = "QUIT";
     private static final ServerCameraSettings cameraSettings = new ServerCameraSettings();
 
     public StartMenuGui(@Nonnull PlayerRef playerRef, @Nonnull CustomPageLifetime lifetime) {
@@ -29,27 +33,36 @@ public class StartMenuGui extends InteractiveCustomUIPage<StartMenuGuiData> {
     @Override
     public void build(@Nonnull Ref<EntityStore> ref, @Nonnull UICommandBuilder uiCommandBuilder, @Nonnull UIEventBuilder uiEventBuilder, @Nonnull Store<EntityStore> store) {
         uiCommandBuilder.append("Pages/start_menu.ui");
-        uiEventBuilder.addEventBinding(CustomUIEventBindingType.Activating, "#PLAYNORMAL", EventData.of("OnButtonClicked", PLAY_NORMAL_BUTTON_ID), false);
+
+        uiEventBuilder.addEventBinding(CustomUIEventBindingType.Activating, "#PLAYCLASSIC", EventData.of("OnButtonClicked", PLAY_NORMAL_BUTTON_ID), false);
+        uiEventBuilder.addEventBinding(CustomUIEventBindingType.Activating, "#PLAYQUICK", EventData.of("OnButtonClicked", PLAY_QUICK_BUTTON_ID), false);
+        uiEventBuilder.addEventBinding(CustomUIEventBindingType.Activating, "#QUIT", EventData.of("OnButtonClicked", QUIT_BUTTON_ID), false);
     }
 
     @Override
     public void handleDataEvent(@Nonnull Ref<EntityStore> ref, @Nonnull Store<EntityStore> store, @Nonnull StartMenuGuiData data) {
         super.handleDataEvent(ref, store, data);
         boolean changed = false;
+        cameraSettings.positionLerpSpeed = 0.2F;
+        cameraSettings.rotationLerpSpeed = 0.2F;
+        cameraSettings.distance = 8.0F;
+        cameraSettings.displayCursor = true;
+        cameraSettings.isFirstPerson = false;
+        cameraSettings.movementForceRotationType = MovementForceRotationType.Custom;
+        cameraSettings.eyeOffset = true;
+        cameraSettings.positionDistanceOffsetType = PositionDistanceOffsetType.DistanceOffset;
+        cameraSettings.rotationType = RotationType.Custom;
+        cameraSettings.rotation = new Direction(0.0F, (-(float)Math.PI / 2F), 0.0F);
 
+        Player player = Objects.requireNonNull(store.getComponent(ref, Player.getComponentType()));
         if (PLAY_NORMAL_BUTTON_ID.equals(data.buttonClicked)) {
-            playerRef.sendMessage(Message.raw("FUCK YES"));
-            cameraSettings.positionLerpSpeed = 0.2F;
-            cameraSettings.rotationLerpSpeed = 0.2F;
-            cameraSettings.distance = 8.0F;
-            cameraSettings.displayCursor = true;
-            cameraSettings.isFirstPerson = false;
-            cameraSettings.movementForceRotationType = MovementForceRotationType.Custom;
-            cameraSettings.eyeOffset = true;
-            cameraSettings.positionDistanceOffsetType = PositionDistanceOffsetType.DistanceOffset;
-            cameraSettings.rotationType = RotationType.Custom;
-            cameraSettings.rotation = new Direction(0.0F, (-(float)Math.PI / 2F), 0.0F);
             playerRef.getPacketHandler().writeNoCache(new SetServerCamera(ClientCameraView.Custom, true, cameraSettings));
+        }
+        else if (PLAY_QUICK_BUTTON_ID.equals(data.buttonClicked)) {
+            playerRef.getPacketHandler().writeNoCache(new SetServerCamera(ClientCameraView.Custom, true, cameraSettings));
+        }
+        else if (QUIT_BUTTON_ID.equals(data.buttonClicked)) {
+            System.exit(0);
         }
 
         if (changed) {
