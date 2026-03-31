d2235d08f366e8e612cf14a529aebad72ede23df
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2026-03-31 13:53:47 +0100

    Spawn Command + Group Change

--- src/main/java/com/kekecreations/overloaded/core/registry/CommandRegistry.java
+++ src/main/java/com/kekecreations/overloaded/core/registry/CommandRegistry.java
@@ -2,11 +2,13 @@ package com.kekecreations.overloaded.core.registry;
 
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.kekecreations.overloaded.common.command.OverloadedTopdownCommand;
+import com.kekecreations.overloaded.common.command.SpawnEntityForRoundCommand;
 
 public class CommandRegistry {
 
     public static void registerCommmands(JavaPlugin javaPlugin) {
         javaPlugin.getCommandRegistry().registerCommand(new OverloadedTopdownCommand());
+        javaPlugin.getCommandRegistry().registerCommand(new SpawnEntityForRoundCommand());
 
     }
 }
