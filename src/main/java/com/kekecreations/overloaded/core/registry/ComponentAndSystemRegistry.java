8ac883d388ef974c3b3010f7928906724f1ea330
KekeCreations <89083260+KekeCreations@users.noreply.github.com>
2026-03-31 14:22:30 +0100

    Registry for systems and components

---
+++ src/main/java/com/kekecreations/overloaded/core/registry/ComponentAndSystemRegistry.java
@@ -0,0 +1,42 @@
+package com.kekecreations.overloaded.core.registry;
+
+import com.hypixel.hytale.component.ComponentType;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+import com.kekecreations.overloaded.common.component.RoundComponent;
+import com.kekecreations.overloaded.common.system.PlayerTickSystem;
+
+public class ComponentAndSystemRegistry {
+
+    private static ComponentType<EntityStore, RoundComponent> roundComponent;
+
+    //private static ComponentType<EntityStore, OtherPlayerRoundComponent> otherRoundComponent;
+
+
+    public static void registerComponents(JavaPlugin javaPlugin) {
+        var registry = javaPlugin.getEntityStoreRegistry();
+        roundComponent = registry.registerComponent(
+                RoundComponent.class,
+                "RoundStats",
+                RoundComponent.CODEC
+        );
+        //Allows to call component from component class
+        RoundComponent.setComponentType(roundComponent);
+
+        /*
+        otherRoundComponent = registry.registerComponent(
+                OtherPlayerRoundComponent.class,
+                "OtherRoundData",
+                OtherPlayerRoundComponent.CODEC
+        );
+        OtherPlayerRoundComponent.setComponentType(otherRoundComponent);
+
+         */
+
+        registry.registerSystem(new PlayerTickSystem(roundComponent));
+       // registry.registerSystem(new OtherPlayerTickSystem(otherRoundComponent));
+        //registry.registerSystem(new PlayerDeathSystem());
+       // registry.registerSystem(new NPCDeathSystem());
+    }
+
+}
